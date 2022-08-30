362e546ee61203f16488c09675562776e19018cb
Grigory <grishavinyar@icloud.com>
2022-08-30 12:51:53 +0300

    Created the project

---
+++ NetworkLayerWithAlamofire/NetworkLayerWithAlamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayerWithAlamofire
+//
+//  Created by Григорий Виняр on 30/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemGreen
+  }
+
+
+}
+
